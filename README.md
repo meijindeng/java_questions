fc66103051054f7977443154a283cb2d1a38748c
dengmeijin <meijindeng@126.com>
2021-05-21 16:04:31 +0800

    2021-5-21：如何将小写字母快速的转换为大写字母

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 ### 1、com.dmj.collection.list
 
 * Test1：怎么将List集合中的元素去重。
+
+### 2、com.dmj.data_type.char_type
+
+* Test1：如何将小写字母快速的转换为大写字母。
---
+++ src/com/dmj/data_type/char_type/Test1.java
@@ -0,0 +1,19 @@
+package com.dmj.data_type.char_type;
+
+/**
+ * 题目：如何将小写字母快速的转换为大写字母
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        // ^ :二进制按位异或。
+        // 97（a）二进制是0110 0001，32对应0010 0000异或结果为0100 0001，转换十进制为65（A）
+        char letter1 = 'a';
+        System.out.println((char)(letter1 ^ 32));
+
+        char letter2 = 'b';
+        System.out.println((char)(letter2 ^ 32));
+
+        char letter3 = 'c';
+        System.out.println((char)(letter3 ^ 32));
+    }
+}
